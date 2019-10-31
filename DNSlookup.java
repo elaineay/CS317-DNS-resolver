711cbede2c572d0013ee5303ffd6490293a23b3a
elaineay <elaineauyang@hotmail.com>
2019-10-30 21:12:02 -0700

    added creation of DNS query

--- DNSlookup.java
+++ DNSlookup.java
@@ -1,10 +1,12 @@
-
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
-
 /**
- * 
+ *
  */
-
 /**
  * @author Donald Acton
  * This example is adapted from Kurose & Ross
@@ -15,6 +17,7 @@ public class DNSlookup {
     
     static final int MIN_PERMITTED_ARGUMENT_COUNT = 2;
     static final int MAX_PERMITTED_ARGUMENT_COUNT = 3;
+    static final int port = 53;
 
     /**
      * @param args
@@ -27,20 +30,22 @@ public class DNSlookup {
         boolean IPV6Query = false;
         InetAddress rootNameServer;
 
+
         if (argCount < MIN_PERMITTED_ARGUMENT_COUNT || argCount > MAX_PERMITTED_ARGUMENT_COUNT) {
             usage();
             return;
         }
 
+        // IP address of DNS server to start search at, may/may not be root DNS server
         rootNameServer = InetAddress.getByName(args[0]);
-        fqdn = args[1];
+        fqdn = args[1]; // fully qualified domain name to look up
 
         if (argCount == 3) {  // option provided
-            if (args[2].equals("-t"))
+            if (args[2].equals("-t")) // print trace of queries, responses, result
                 tracingOn = true;
-            else if (args[2].equals("-6"))
+            else if (args[2].equals("-6")) // retrieve IPV6 address
                 IPV6Query = true;
-            else if (args[2].equals("-t6")) {
+            else if (args[2].equals("-t6")) { // trace and IPV6
                 tracingOn = true;
                 IPV6Query = true;
             } else  { // option present but wasn't valid option
@@ -48,10 +53,55 @@ public class DNSlookup {
                 return;
             }
         }
-
     }
 
 	// Start adding code here to initiate the lookup
+
+    public static void lookup(InetAddress rootNameServer, String fqdn) throws IOException {
+        ByteArrayOutputStream bAOutput = new ByteArrayOutputStream();
+        DataOutputStream dOutput = new DataOutputStream(bAOutput);
+
+        // REFERENCE https://stackoverflow.com/questions/36743226/java-send-udp-packet-to-dns-server
+        // building DNS request
+        dOutput.writeShort(0x1234); // Transaction ID of query (16 bits)
+        dOutput.writeShort(0x0100); // flag, standard query
+        dOutput.writeShort(0x0001); // # questions
+        dOutput.writeShort(0x0000); // # answer records
+        dOutput.writeShort(0x0000); // # authority records
+        dOutput.writeShort(0x0000); // # additional records
+
+        // change into bytes
+        // REFERENCE https://beginnersbook.com/2013/12/java-string-getbytes-method-example/
+        String[] dnSections = fqdn.split("\\.");
+        dOutput.writeByte(dnSections.length);
+
+        // write the domain into the DNS request
+        for (String s : dnSections) {
+            byte[] byteArray = s.getBytes("UTF-8");
+            dOutput.write(byteArray);
+        }
+
+        dOutput.writeByte(0x00); //signify end of DNS request
+        dOutput.writeShort(0x0001); // record type A (host request)
+        dOutput.writeShort(0x0001); // class IN
+
+        byte[] outputFrame = bAOutput.toByteArray();
+
+        // Send DNS Request
+        DatagramSocket socket = new DatagramSocket();
+        DatagramPacket packet = new DatagramPacket(outputFrame,
+                                                   outputFrame.length,
+                                                   rootNameServer,
+                                                   port);
+        socket.send(packet);
+
+        // TODO: stopped here, have not yet called lookup in main, need to do waiting for response from server
+    }
+
+    // lookup should print name, space, TTL, 3 spaces, type, space, resolved IP address
+    // IPv4 address: type "A"
+    // IPV6 address: type "AAA"
+    //    System.out.println();
 	
     
     private static void usage() {
