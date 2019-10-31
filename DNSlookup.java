f5270bbb218499604fc13f2f86581b7941ee3c0e
elaineay <elaineauyang@hotmail.com>
2019-10-30 19:07:11 -0700

    fix tabs lol

--- DNSlookup.java
+++ DNSlookup.java
@@ -1,7 +1,5 @@
 
-import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.Random;
 
 /**
  * 
@@ -22,34 +20,34 @@ public class DNSlookup {
      * @param args
      */
     public static void main(String[] args) throws Exception {
-	String fqdn;
-	DNSResponse response; // Just to force compilation
-	int argCount = args.length;
-	boolean tracingOn = false;
-	boolean IPV6Query = false;
-	InetAddress rootNameServer;
-	
-	if (argCount < MIN_PERMITTED_ARGUMENT_COUNT || argCount > MAX_PERMITTED_ARGUMENT_COUNT) {
-	    usage();
-	    return;
-	}
-	
-	rootNameServer = InetAddress.getByName(args[0]);
-	fqdn = args[1];
-	
-	if (argCount == 3) {  // option provided
-	    if (args[2].equals("-t"))
-		tracingOn = true;
-	    else if (args[2].equals("-6"))
-		IPV6Query = true;
-	    else if (args[2].equals("-t6")) {
-		tracingOn = true;
-		IPV6Query = true;
-	    } else  { // option present but wasn't valid option
-		usage();
-		return;
-	    }
-	}
+        String fqdn;
+        DNSResponse response; // Just to force compilation
+        int argCount = args.length;
+        boolean tracingOn = false;
+        boolean IPV6Query = false;
+        InetAddress rootNameServer;
+
+        if (argCount < MIN_PERMITTED_ARGUMENT_COUNT || argCount > MAX_PERMITTED_ARGUMENT_COUNT) {
+            usage();
+            return;
+        }
+
+        rootNameServer = InetAddress.getByName(args[0]);
+        fqdn = args[1];
+
+        if (argCount == 3) {  // option provided
+            if (args[2].equals("-t"))
+                tracingOn = true;
+            else if (args[2].equals("-6"))
+                IPV6Query = true;
+            else if (args[2].equals("-t6")) {
+                tracingOn = true;
+                IPV6Query = true;
+            } else  { // option present but wasn't valid option
+                usage();
+                return;
+            }
+        }
 
     }
 
@@ -57,14 +55,14 @@ public class DNSlookup {
 	
     
     private static void usage() {
-	System.out.println("Usage: java -jar DNSlookup.jar rootDNS name [-6|-t|t6]");
-	System.out.println("   where");
-	System.out.println("       rootDNS - the IP address (in dotted form) of the root");
-	System.out.println("                 DNS server you are to start your search at");
-	System.out.println("       name    - fully qualified domain name to lookup");
-	System.out.println("       -6      - return an IPV6 address");
-	System.out.println("       -t      - trace the queries made and responses received");
-	System.out.println("       -t6     - trace the queries made, responses received and return an IPV6 address");
+        System.out.println("Usage: java -jar DNSlookup.jar rootDNS name [-6|-t|t6]");
+        System.out.println("   where");
+        System.out.println("       rootDNS - the IP address (in dotted form) of the root");
+        System.out.println("                 DNS server you are to start your search at");
+        System.out.println("       name    - fully qualified domain name to lookup");
+        System.out.println("       -6      - return an IPV6 address");
+        System.out.println("       -t      - trace the queries made and responses received");
+        System.out.println("       -t6     - trace the queries made, responses received and return an IPV6 address");
     }
 }
 
